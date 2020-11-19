0a2d4a22456da1c7a12d902d9555b118c92a4bb5
SakiiR SakiiR <sakiirlessons@gmail.com>
2020-11-19 17:05:29 +0100

    Removed the opt directory

--- 2020-11-14-BalsnCTF/tpc/files/opt/workdir/docker-entrypoint-dc1e2f5f7a4f359bb5ce1317a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-exec gunicorn 'main-dc1e2f5f7a4f359bb5ce1317a:app' \
-    --bind '0.0.0.0:8000' \
-    --workers 5 \
-    --worker-tmp-dir "/dev/shm" \
-    --worker-class "gevent" \
-    --access-logfile "-" \
-    --error-logfile "-"
-
